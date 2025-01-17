a8003ef37ca2b97541470fae839954a2441377a4
Shaurya Gupta <guptashaurya0728@gmail.com>
2025-01-17 18:10:26 +0530

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter , Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage/Homepage';
 import Sidebar from './components/sidebar/Sidebar';
 import Main from './components/main/Main';
@@ -7,7 +7,7 @@ import ImageGenerator from './components/GenerateImage/ImageGenerator';
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/app" element={<><Sidebar /><Main /></>} />
@@ -15,7 +15,7 @@ const App = () => {
          <Route path="/login" element={<div>Login Page</div>} /> {/* Replace with your actual Login component */}
         <Route path="/signup" element={<div>Sign Up Page</div>} /> {/* Replace with your actual Sign-Up component */}
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
